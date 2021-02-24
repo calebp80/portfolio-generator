a483d4cc5ba1d274a46ed0c847efc06a9b8d9d65
calebp80 <74808310+calebp80@users.noreply.github.com>
2021-02-23 21:35:09 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //const profileDataArgs = process.argv.slice(2, process.argv.length);
 //console.log(profileDataArgs);
 
-const generatePage = () => 'Name: Caleb, Github: calebp80';
-console.log(generatePage());
+const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+console.log(generatePage('Caleb', 'calebp80'));
