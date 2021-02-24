cd1db86b9f804df31b154cd3bb73c883179e7450
calebp80 <74808310+calebp80@users.noreply.github.com>
2021-02-23 21:04:12 -0800

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ const printProfileData = profileDataArr => {
   console.log('================');
 
   // Is the same as this...
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-  });
-};
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+  
 
 printProfileData(profileDataArgs);
