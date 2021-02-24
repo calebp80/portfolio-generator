c445591f32249b7c9059405afb8ae61b47068b91
calebp80 <74808310+calebp80@users.noreply.github.com>
2021-02-23 17:06:03 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-Const PrintProfileData = profileDataArr => {
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
   for (let i = 0; i < profileDataArr.length; i++) {
     console.log(profileDataArr[i]);
   }
