253ac2487ed1b045fc00e83f61785d9f806556d3
calebp80 <74808310+calebp80@users.noreply.github.com>
2021-02-23 17:04:01 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const printProfileData = profileDataArr => {
+Const PrintProfileData = profileDataArr => {
   for (let i = 0; i < profileDataArr.length; i++) {
     console.log(profileDataArr[i]);
   }
