4a5af6936681b7f91d486d9ac99fcc72149d056f
calebp80 <74808310+calebp80@users.noreply.github.com>
2021-02-23 20:51:46 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const message = 'Hello Node!';
 
-const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i++) {
-  console.log(profileDataArr[i]);
-  }
-};
+if (true === true) {
+  const message = 'Hello ES6!';
+  let sum = 5;
+  sum += 10;
+  console.log(message);
+  console.log(sum);
+}
 
-printProfileData(profileDataArgs);
+console.log(message);
+console.log(sum);
